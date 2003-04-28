82a37dcaddc0f56ea561026bd42a0111af4486a4
philburk <philburk@0f58301d-fd10-0410-b4af-bbb618454e57>
2003-04-28 17:43:48 +0000

    removed spurious 'F' in license comment

--- portaudio/branches/V18-patch/pa_win_wmme/pa_win_wmme.c
+++ portaudio/branches/V18-patch/pa_win_wmme/pa_win_wmme.c
@@ -9,7 +9,7 @@
  * Authors: Ross Bencina and Phil Burk
  * Copyright (c) 1999-2000 Ross Bencina and Phil Burk
  *
- * Permission is hereby granted, free of charge, to any person obtainingF
+ * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files
  * (the "Software"), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge,
